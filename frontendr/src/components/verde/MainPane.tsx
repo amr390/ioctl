5cd83ecf32a563a39521f23180cf2b0798e8060f
amr <dquijote22@gmail.com>
2022-10-14 18:44:00 +0200

    shadow

--- frontendr/src/components/verde/MainPane.tsx
+++ frontendr/src/components/verde/MainPane.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from 'react'
 
 export const MainPane = (props: PropsWithChildren<any>) => {
   return (
-      <main role="main" className="relative m-auto h-[90vh] z-10 flex-grow pt-1 px-3 bg-white shadow-lg hover:shadow-md shadow-gray-200 hover:shadow-gray-300">
+      <main role="main" className="relative m-auto h-[90vh] z-10 flex-grow pt-1 px-3 bg-white shadow-[0_0_10px_rgb(0,0,0,0.2)] shadow-gray-200 hover:shadow-gray-300">
         {props.children}
       </main>
   )
