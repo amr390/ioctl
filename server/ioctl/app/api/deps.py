fdb39a72340d76003df775c55003fff2a20cd50d
amr <anma@manning.com>
2022-10-31 08:46:49 +0100

    dev

--- server/ioctl/app/api/deps.py
+++ server/ioctl/app/api/deps.py
@@ -13,7 +13,7 @@ from app.db.session import SessionLocal
 
 
 reusable_oauth2 = OAuth2PasswordBearer(
-    tokenUrl=f"{settings.API_V1_STR}/login/access-token"
+    tokenUrl=f"{settings.API_V1_STR}/auth/signin"
 )
 
 
