64e312c3f4f273c41620a232351555da9ab80f66
amr <dquijote22@gmail.com>
2022-11-28 23:53:48 +0100

    using refresh hook

---
+++ frontendr/src/hooks/useRefreshToken.ts
@@ -0,0 +1,24 @@
+import axios from '@utils/axios'
+import { API_ROUTES } from '@utils/constants'
+import { AxiosResponse } from 'axios'
+import { useAuthenticated } from './useAuthenticated'
+
+const setAuth = (prev: any) => {}
+
+const useRefreshToken = () => {
+  const refresh = async () => {
+    const response: AxiosResponse = await axios.get(API_ROUTES.SIGN_REFRESH, {
+      withCredentials: true,
+    })
+
+    setAuth((prev: any) => {
+      console.log('some code that lets you set the refresh token in a cookie')
+      console.log('some code that sets the jwt token in a service')
+      return { ...prev, accessToken: response.data.accessToken }
+    })
+  }
+
+  return refresh;
+}
+
+export default useRefreshToken
--- frontendr/src/pages/users.tsx
+++ frontendr/src/pages/users.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import { FC, useEffect, useState } from 'react'
 import { useAuthenticated } from '@hooks/useAuthenticated'
+import axios from '@utils/axios'
+import { API_ROUTES } from '@utils/constants'
+import { AxiosResponse } from 'axios'
 
-export const users = () => {
-  const { authenticated } = useAuthenticated();
-  return (
-    <div>
+export default function Users() {
+  const [users, setUsers] = useState([])
 
-    </div>
+  useEffect(() => {
+    let isMounted = true 
+    const controller = new AbortController()
+
+    const getUsers = async () => {
+      try {
+        const response: AxiosResponse = await axios.get(API_ROUTES.USER_CRUD, {
+          signal: controller.signal,
+        })
+        console.log('user list: ', response.data)
+
+        isMounted && setUsers(response.data)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    getUsers()
+
+    return () => {
+      controller.abort() // abort any requests on going
+      isMounted = false // don't let set any state once the component is unmountejjkk
+    }
+  }, [])
+  return (<>
+    <h2>User list</h2>
+    <article>
+      {users && users.map((user, idx) => <div key={idx}> {user}</div>)}
+    </article></>
   )
 }
