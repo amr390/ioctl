914375c482f37cf7009c0c64a885ebc2b05530a1
amr <dquijote22@gmail.com>
2022-12-09 09:31:46 +0100

    side menu

--- frontendr/src/components/common/LeftPane.tsx
+++ frontendr/src/components/common/LeftPane.tsx
@@ -17,22 +17,22 @@ export const LeftPane = (props: {}) => {
         </article>
         <article className='flex flex-col p-2 w-full items-center rounded-sm'>
           <h2 className='font-bold txt-lg'>Views</h2>
-          <div className='flex flex-row justify-between px-2 w-full items-center'>
-            <div className='flex items-start'>
-              <Link href='/login'>Login</Link>
-            </div>
-          </div>
-
-          <div className='flex flex-row justify-between px-2 w-full items-center'>
-            <div className='flex items-start'>
-              <Link href='/'>Home</Link>
-            </div>
-          </div>
-
-          <div className='flex flex-row justify-between px-2 w-full items-center'>
-            <div className='flex items-start'>
-              <Link href='/profile'>Profile</Link>
-            </div>
+          <div className='flex justify-center w-full'>
+            <ul className='bg-white rounded-lg border border-gray-200 w-96 text-gray-900'>
+              <li className='px-6 py-2 border-b border-gray-200 w-full rounded-t-md bg-black text-white'>
+                Profile
+              </li>
+              <li className='px-6 py-2 border-b border-gray-200 w-full'>
+                Dashboard
+              </li>
+              <li className='px-6 py-2 border-b border-gray-200 w-full'>
+                Tasks
+              </li>
+              <li className='px-6 py-2 border-b border-gray-200 w-full'>
+                Admin
+              </li>
+              <li className='px-6 py-2 w-full rounded-b-lg'>Settings</li>
+            </ul>
           </div>
         </article>
       </section>
