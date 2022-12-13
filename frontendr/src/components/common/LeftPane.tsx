af41a5dec9c4d2e9be925058a620c096219b310f
amr <dquijote22@gmail.com>
2022-12-13 08:35:27 +0100

    if authenticated, show it in the menu

--- frontendr/src/components/common/LeftPane.tsx
+++ frontendr/src/components/common/LeftPane.tsx
@@ -1,16 +1,22 @@
-import Link from 'next/link'
+import { useAuth } from '@hooks/useAuth'
 import React from 'react'
 
-export const LeftPane = (props: {}) => {
+export const LeftPane = () => {
+  const { auth } = useAuth()
+
+  const signedInStyle =
+    'flex justify-center items-center w-20 h-6 rounded-lg text-black text-xs font-semibold bg-yellow-400'
+  const notSignedInStyle =
+    'flex justify-center items-center w-20 h-6 rounded-lg text-black text-xs font-semibold bg-gray-300'
   return (
     <div className='w-fixed h-screen w-1/4 flex-shrink flex-grow-0 p-4 bg-white'>
       <section className='border bg-gray-50 border-gray-100 h-full'>
         <article className='flex flex-col p-2 w-full h-18 items-center'>
           <div className='flex relative top-[-10px] w-full h-16 justify-center'>
-            <div className='flex flex-row justify-between items-center px-2 border rounded-md w-11/12 bg-black text-white text-center'>
+            <div className='flex flex-row justify-between items-center px-1 border rounded-md w-11/12 bg-black text-white text-center'>
               <span className='text-white text-sm '>Current status</span>
-              <span className='flex justify-center items-center w-20 h-6 rounded-xl text-black text-xs font-semibold bg-gray-300'>
-                LOGOUT
+              <span className={auth.access_token ? signedInStyle : notSignedInStyle}>
+                {auth.access_token ? 'SIGNED IN' : 'NOT SIGNED'}
               </span>
             </div>
           </div>
