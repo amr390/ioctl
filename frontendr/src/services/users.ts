22f6037ffb042ff483dc83616af6bcee16c66448
amr <dquijote22@gmail.com>
2023-02-07 14:49:06 +0100

    services for api calls

---
+++ frontendr/src/services/users.ts
@@ -0,0 +1,19 @@
+
+
+
+const getMe = async ()=> {} 
+const list = async ()=> {} 
+const save = async ()=> {}
+const update = async ()=> {} 
+const remove = async ()=> {} 
+
+
+const UserApi = {
+  getMe,
+  list,
+  save,
+  update,
+  remove,
+}
+
+export default UserApi;
