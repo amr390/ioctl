c0a1f7ac798d7d0f52e5e0a5ce0b4abd46de1dc9
amr <dquijote22@gmail.com>
2023-02-17 09:43:58 +0100

    remove menu provider

--- frontendr/src/pages/_app.tsx
+++ frontendr/src/pages/_app.tsx
@@ -1,20 +1,17 @@
 import { Layout } from '@components/common/_layout'
 import { AuthProvider } from '@context/AuthProvider'
-import { MenuProvider } from '@context/MenuProvider'
 import type { AppProps } from 'next/app'
 import { Toaster } from 'react-hot-toast'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MenuProvider>
       <AuthProvider>
         <Layout>
           <Toaster />
           <Component {...pageProps} />
         </Layout>
       </AuthProvider>
-    </MenuProvider>
   )
 }
 
