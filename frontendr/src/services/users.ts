80878fde9c0c7150a9adbe33b145c476645b8dee
amr <dquijote22@gmail.com>
2023-08-23 18:59:34 +0200

    formatting users service

--- frontendr/src/services/users.ts
+++ frontendr/src/services/users.ts
@@ -4,28 +4,32 @@ import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
 
 /* Wrapper to store all methods connecting to user resource api */
 
-const getMe = async (axios: AxiosInstance, opts: AxiosRequestConfig): Promise<AxiosResponse> => {
+const getMe = async (
+  axios: AxiosInstance,
+  opts: AxiosRequestConfig
+): Promise<AxiosResponse> => {
   return axios.get(API_ROUTES.USER_ME_GET, opts)
 }
 const updateMe = async (axios: AxiosInstance, user: IUser) => {
-  const headers = { headers: { 'content-type': 'application/json', }}
-  const response =  await axios.put(`${API_ROUTES.USER_ME_PUT}`, user, headers)
+  const headers = { headers: { 'content-type': 'application/json' } }
+  const response = await axios.put(`${API_ROUTES.USER_ME_PUT}`, user, headers)
   return response.data
 }
 
-const list = async (axios: AxiosInstance, opts: AxiosRequestConfig): Promise<AxiosResponse> => {
+const list = async (
+  axios: AxiosInstance,
+  opts: AxiosRequestConfig
+): Promise<AxiosResponse> => {
   return axios.get(API_ROUTES.USER_CRUD, opts)
 }
 const save = async (axios: AxiosInstance, user: IUser) => {
   return axios.post(API_ROUTES.USER_CRUD, user, {
-    headers: {
-      'content-type': 'application/json',
-    },
+    headers: { 'content-type': 'application/json' },
   })
 }
 
 const update = async (axios: AxiosInstance, user: IUser) => {
-  const headers = { headers: { 'content-type': 'application/json', }}
+  const headers = { headers: { 'content-type': 'application/json' } }
   return axios.put(`${API_ROUTES.USER_CRUD}/${user.id}`, user, headers)
 }
 const remove = async (axios: AxiosInstance, user: IUser) => {
